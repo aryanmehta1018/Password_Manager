af274129fb75b0b08862168ed15fa5a1221de568
aryanmehta1018 <ARYAN.23FE10CSE00360@muj.manipal.edu>
2025-05-13 18:53:58 +0530

    Update config.py

--- password_manager/config.py
+++ password_manager/config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'abcdefg')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:ronaldorox@localhost/password_manager'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:pass@localhost/password_manager'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
