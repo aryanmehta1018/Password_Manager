4f2c2aef2f6cdb7ed8d10896638e75c85d4b9db9
aryanmehta1018 <mehtaaryan1018@gmail.com>
2026-02-10 21:17:07 +0530

    Delete password_manager directory

--- password_manager/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'abcdefg')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:pass@localhost/password_manager'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
